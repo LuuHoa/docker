02502559913d3f4bdd59c2f24dbb93f8cae8c48e
Hoa Luu <luukimhoa@gmail.com>
2023-07-28 23:52:46 -0400

    Create install_mac_m1.sh

---
+++ k82/Minikube/install_mac_m1.sh
@@ -0,0 +1,4 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-arm64
+sudo install minikube-darwin-arm64 /usr/local/bin/minikube
+
+minikube start --driver=docker --alsologtostderr
